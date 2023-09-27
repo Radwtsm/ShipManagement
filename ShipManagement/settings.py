59b88638f9138893fd929a2935ecb2ed480a969c
radwtsm <radwtsm@gmail.com>
2023-09-27 21:08:32 +0200

    useful files

--- ShipManagement/settings.py
+++ ShipManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y&e(z(7)icx3k(b+!9kz)pchgh^tsz@2d@mghrarhtm+j)w114
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
